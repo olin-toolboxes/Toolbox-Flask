02622abbefa3c740ef87df3a42876c2d22550048
Shreya Rangarajan <Shreya.Rangarajan@students.olin.edu>
2017-10-31 00:45:50 -0400

    Finished Flask Toolbox

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,28 @@
 """
 Put your Flask app code here.
 """
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def profile():
+    for key in request.form.keys():
+        if request.form[key] is ' ':
+            return "Error, please input all form fields"
+    return render_template('info.html',
+                            name_user = request.form['name'],
+                            age_user = request.form['age'],
+                            ninja = request.form['fav_ninja'])
+
+    #check they exist
+    #send to profile html page
+
+
+if __name__ == '__main__':
+    app.run()
