6022781cdc1ce21a737307cc63686f006a039379
Isabel Blancett <isabel.blancett@students.olin.edu>
2017-11-01 22:53:50 -0400

    Completed Flask Toolbox

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,31 @@
 """
 Put your Flask app code here.
 """
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    make_profile()
+    return render_template('survey.html')
+
+@app.route('/user', methods=['GET', 'POST'])
+def make_profile():
+    print('hello')
+    try:
+        name = request.form['name']
+        age = request.form['age']
+        return render_template('profile.html', name=name, age=age)
+    except:
+        return
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
